29d945c44d309a220a7a33a2dedc651307c24b6f
proMonkey <lcx19990918@163.com>
2021-03-05 20:44:59 +0800

    fix: dockerfile error

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def test_service():
-    return 'Hello, Welcome to CloudBase!\n'
+    return 'Hello!!!!!!\n'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
